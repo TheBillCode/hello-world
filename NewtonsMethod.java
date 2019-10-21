258c68e3f8147958ae3d018f782421fa06e3d4a2
Bill <55706426+TheBillCode@users.noreply.github.com>
2019-10-21 18:42:02 -0400

    Add files via upload

---
+++ NewtonsMethod.java
@@ -0,0 +1,18 @@
+package newtonsmethod;
+
+//can recursion be used to find roots using Newton's Method? 
+
+public class NewtonsMethod {
+    final static double nthPlace = 0.0000000000001; //accuracy
+
+    public static void main(String[] args) {
+        System.out.println(getNewton(3, 2, 0)); //x, start guess, blank
+    }
+    
+    public static double getNewton(double x, double n, double n2) {
+        if (n == 0 | Math.abs(n-n2) < nthPlace) return n;
+        n2=n;
+        n = n-(((n*n)-x)/(x*n));
+        return getNewton(x, n, n2);
+    }
+}
